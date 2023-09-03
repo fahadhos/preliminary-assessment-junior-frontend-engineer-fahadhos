a44dd87578e66ee743ca22ecddcf07f1ec5e392e
fahadhos <generalus159@gmail.com>
2023-09-03 14:43:19 +0600

    Add:  src all folders

--- src/routes/layout/+page.svelte
+++ src/routes/layout/+page.svelte
@@ -1,5 +1,9 @@
 <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  
- 
+Hello world!
+</h1>
+
+<style lang="postcss">
+:global(html) {
+    background-color: theme(colors.gray.100);
+}
+</style>
